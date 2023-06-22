8631830fd185e4d6f82769da4f906bb0664433b5
Adrian Mayfield <amay0808@protonmail.com>
2023-06-22 05:52:01 -0700

    open and close modal

--- src/App.js
+++ src/App.js
@@ -4,38 +4,46 @@ import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
 import ModalWithForm from "./ModalWithForm/ModalWithForm";
+import { useState } from "react";
 
 function App() {
   const weatherTemp = "75°F";
+  const [activeModal, setActiveModal] = useState("");
+  const handleCreateModal = () => {
+    setActiveModal("create");
+  };
   return (
     <div>
-      <Header />
+      <Header onCreateModal={handleCreateModal} />
       <Main weatherTemp={weatherTemp} />
       <Footer />
-      <ModalWithForm title="New Garment">
-        <label>
-          name <input type="text" name="name" minLength="1" maxLength="30" />
-        </label>
-        <label>
-          Image <input type="url" name="name" minLength="1" maxLength="30" />
-        </label>
-        <p>Select the weather type: </p>
-        <div>
+      {activeModal === "create" && (
+        <ModalWithForm title="New Garment">
+          <label>
+            name <input type="text" name="name" minLength="1" maxLength="30" />
+          </label>
+          <label>
+            Image <input type="url" name="name" minLength="1" maxLength="30" />
+          </label>
+          <p>Select the weather type: </p>
           <div>
-            <input type="radio" id="hot" value="hot" />
-            <label>Hot</label>
+            <div>
+              <input type="radio" id="hot" value="hot" />
+              <label>Hot</label>
+            </div>
+            <div>
+              <input type="radio" id="warm" value="warm" />
+              <label>Warm</label>
+            </div>
+            <div>
+              <input type="radio" id="cold" value="cold" />
+              <label>Cold</label>
+            </div>
           </div>
-          <div>
-            <input type="radio" id="warm" value="warm" />
-            <label>Warm</label>
-          </div>
-          <div>
-            <input type="radio" id="cold" value="cold" />
-            <label>Cold</label>
-          </div>
-        </div>
-      </ModalWithForm>
+        </ModalWithForm>
+      )}
     </div>
   );
 }
+
 export default App;
