6bb80a8d596a4b787e4bc62debdcbbc50210838e
Adrian Mayfield <amay0808@protonmail.com>
2023-06-25 14:16:18 -0700

    chagne import path from Utils to utils

---
+++ src/utils/Weatherapi.js
@@ -0,0 +1,27 @@
+const latitude = 37.3022;
+const longitude = -120.4829;
+const APIkey = "ec819665b8e9b69fc9cee9d11ea26e49";
+
+export const getForecastWeather = async () => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.error("An error occurred while fetching the weather data", error);
+    throw error;
+  }
+};
+
+export const parseWeatherData = (data) => {
+  const main = data.main;
+  const temperature = main && main.temp;
+  return Math.ceil(temperature);
+};
