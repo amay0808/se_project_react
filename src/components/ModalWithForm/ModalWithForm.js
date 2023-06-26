85e6e2094f758c4d911c4587b9d46bff777352d7
Adrian Mayfield <amay0808@protonmail.com>
2023-06-26 11:03:12 -0700

    move submit button inside form tag

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -17,10 +17,12 @@ const ModalWithForm = ({
           className="modal__close-button"
         />
         <h3 className="modal__title">{title}</h3>
-        <form>{children}</form>
-        <button className="add__garment-button" type="submit">
-          {buttonText}
-        </button>
+        <form>
+          {children}
+          <button className="add__garment-button" type="submit">
+            {buttonText}
+          </button>
+        </form>
       </div>
     </div>
   );
