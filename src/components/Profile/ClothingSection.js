635b887db826c7614771ebf64e39afe5bd60e5e0
Adrian Mayfield <amay0808@protonmail.com>
2023-07-10 00:39:41 -0700

    position profile clothing items

--- src/components/Profile/ClothingSection.js
+++ src/components/Profile/ClothingSection.js
@@ -2,26 +2,27 @@ import React from "react";
 import "../Profile/clothingsection.css";
 
 function ClothesSection({ clothingItems, onCreateModal, onSelectCard }) {
-  // Add onSelectCard prop
   return (
-    <div className="clothes-section">
-      <div className="clothes-section__header">
-        <h2 className="clothes-section__title">Your Items</h2>
-        <button className="clothes-section__button" onClick={onCreateModal}>
+    <div className="clothes">
+      <div className="clothes__header">
+        <h2 className="clothes__title">Your Items</h2>
+        <button className="clothes__btn" onClick={onCreateModal}>
           + Add New
         </button>
       </div>
-      {clothingItems.map((item) => (
-        <div key={item.id} className="card">
-          <span className="card__text">{item.name}</span>
-          <img
-            className="card__image"
-            src={item.imageUrl}
-            alt={item.name}
-            onClick={() => onSelectCard(item)} // Add this line
-          />
-        </div>
-      ))}
+      <div className="cardCont">
+        {clothingItems.map((item) => (
+          <div key={item.id} className="card">
+            <span className="card__text">{item.name}</span>
+            <img
+              className="card__image"
+              src={item.imageUrl}
+              alt={item.name}
+              onClick={() => onSelectCard(item)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Profile/Sidebar.js
+++ src/components/Profile/Sidebar.js
@@ -5,12 +5,17 @@ function Sidebar({ user }) {
   const { username, avatar } = user;
 
   return (
-    <div className="sidebar">
-      <div className="sidebar__info">
-        <h2 className="sidebar__user-title">{username}</h2>
-        <img className="sidebar__user-avatar" src={avatar} alt="User Avatar" />
+    <div className="profile-sidebar">
+      <div className="profile-sidebar__info">
+        <h2 className="profile-sidebar__user-title">{username}</h2>
+        <img
+          className="profile-sidebar__user-avatar"
+          src={avatar}
+          alt="User Avatar"
+        />
       </div>
     </div>
   );
 }
+
 export default Sidebar;
