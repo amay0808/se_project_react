901e20af55f3a60212d5573be2f9620d6d639630
Adrian Mayfield <amay0808@protonmail.com>
2023-07-11 00:05:58 -0700

    include sidebar in profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,13 @@
 //Profile.js
 import React from "react";
+import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 import "./Profile.css";
 
-function Profile({ user, clothingItems, onCreateModal, onSelectCard }) {
+function Profile({ clothingItems, onCreateModal, onSelectCard }) {
   return (
     <div className="profile-container">
+      <SideBar />
       <ClothesSection
         clothingItems={clothingItems}
         onCreateModal={onCreateModal}
