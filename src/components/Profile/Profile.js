ab63f304952ce1ecf3d64eda09e47211832df222
Adrian Mayfield <amay0808@protonmail.com>
2023-07-11 00:03:21 -0700

    remove sidebar from profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,11 @@
 //Profile.js
 import React from "react";
-import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 import "./Profile.css";
 
 function Profile({ user, clothingItems, onCreateModal, onSelectCard }) {
   return (
     <div className="profile-container">
-      <SideBar user={user} />
       <ClothesSection
         clothingItems={clothingItems}
         onCreateModal={onCreateModal}
